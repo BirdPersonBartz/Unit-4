e5c7e048e92cc8e889da181647c4200590bf9edd
BirdPersonBartz <griffbartzen@gmail.com>
2016-07-24 19:03:06 -0400

    not sure why the f1 won't work

--- cv.py
+++ cv.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[10]:
+# In[24]:
 
 from sklearn import datasets, cross_validation, svm
 import numpy as np
 import pandas as pd
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score, make_scorer
 
 
 iris = datasets.load_iris()
@@ -28,13 +28,13 @@ X = df[[0,1,2,3]].values
 y = df['iris type'].values
 
 
-# In[11]:
+# In[25]:
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(
     X, y, test_size=0.4, random_state=42)
 
 
-# In[15]:
+# In[26]:
 
 svc = svm.SVC(kernel='linear')
 svc.fit(X_train, y_train)
@@ -42,7 +42,7 @@ svc.fit(X_train, y_train)
 results = svc.predict(X_test)
 
 
-# In[16]:
+# In[27]:
 
 correct_count = 0
 incorrect_count = 0
@@ -50,15 +50,36 @@ incorrect_count = 0
 print(accuracy_score(y_test, results))
 
 
-# In[18]:
+# In[30]:
 
 
 cv_score_5 = cross_validation.cross_val_score(svc,X,y,cv=5)
 
 
-# In[19]:
+# In[33]:
 
-print(cv_score)
+print('cross val score %s' % cv_score)
+print('mean %s' % cv_score.mean())
+print('variance %s' % cv_score.std()**2)
+
+
+# In[35]:
+
+#cv_score_5_f1 = cross_validation.cross_val_score(svc,X,y,cv=5, scoring = make_scorer(f1_score, average = None))
+#not sure why this doesn't work
+
+
+cv_score_5_f1 = f1_score(y_test, results, average=None)
+
+
+# In[36]:
+
+print(cv_score_5_f1)
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
